dd895b22183edf804c88c17c5bfc655ee5fbcf72
samplevivekgithub <vivekanandgy@gmail.com>
2026-02-08 12:14:04 +0530

    Initial commit

---
+++ BlogApplication (1)/BlogApplication/src/main/java/com/blog/utils/AppProperties.java
@@ -0,0 +1,9 @@
+package com.blog.utils;
+
+
+public class AppProperties 
+{
+   public static final String CLOUDINARY_CLOUD_NAME ="VKBlog";
+   public static final String CLOUDINARY_API_KEY="945231658788687";
+   public static final String CLOUDINARY_SECRET_KEY="4gK3__hKu15Xkfqj318OTjuFjtE";
+}
